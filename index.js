154d1f84c721154f75ffefd0400946cbdcfd83e7
0xCipher <64740712+0xCipher-max@users.noreply.github.com>
2022-09-19 15:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ $(document).scroll(function()
    }
 
 })
+// Do not try to edit please
+var form = document.getElementById("my-form");
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Thanks for your submission!";
+      status.style.textAlign = "center";
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.style.backgroundColor = "#ff7878";
+    });
+}
+form.addEventListener("submit", handleSubmit);
