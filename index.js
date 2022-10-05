b234cf5d548a0be3e338e506a35266bf7983015f
tanishqJain16 <103594976+tanishqJain16@users.noreply.github.com>
2022-10-05 11:37:26 +0530

    changes in navbar, teampage and contact form
    added active identification in navbar, added content in teampage and added css to form submission message

--- index.js
+++ index.js
@@ -59,31 +59,4 @@ let noOfCharac = 600;
       }
 
 
-// Do not try to edit please
-var form = document.getElementById("my-form");
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      status.innerHTML = "Thanks for your submission!";
-      status.style.textAlign = "center";
-      status.style.marginTop="5%";
-      status.style.fontWeight="800";
-      status.style.fontSize="larger";
-      status.style.color="#00629b";
-      form.reset();
-    })
-    .catch((error) => {
-      status.innerHTML = "Oops! There was a problem submitting your form";
-      status.style.backgroundColor = "#ff7878";
-    });
-}
-form.addEventListener("submit", handleSubmit);
+
